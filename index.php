34390d41e187fa372778b59270c7aa4b0372a349
Nathan Tapia-Ramirez <ntapiaramirez@outlook.com>
2024-05-02 13:41:01 -0400

    Updated controller, default route points to survey for user

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 require_once ('vendor/autoload.php');
-
+require_once ('model/data-layer.php');
 
 $F3 = Base::instance();
 
@@ -14,15 +14,35 @@ $F3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 
-// Summary Route
-$F3->route('GET|POST /summary', function($F3){
+// Survey Route
+$F3->route('GET|POST /survey', function($F3){
     var_dump($F3->get('SESSION'));
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        //var_dump($_POST);
+        // Get the data from the post array
+        if (isset($_POST['colors']))
+            $colors = implode(", ", $_POST['colors']);
+        else
+            $colors = "None selected";
+        // If the data valid
+        if (true) {
+            // Add the data to the session array
+            $F3->set('SESSION.colors', $colors);
+            $F3->set('SESSION.name', $name);
+            // Send the user to the next form
+            $F3->reroute('summary');
+        }
+        else {
+            // Temporary
+            echo "<p>Validation errors</p>";
+        }
+    }
+    $colors = getColors();
+
+    $F3->set('colors', $colors);
     $view=new Template();
-    echo $view->render('view/summary.html');
+    echo $view->render('views/survey.html');
 });
-
-
-
 
 
 $F3->run();
---
+++ model/data-layer.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/* This is my Data Layer.
+ * It belongs to the Model.
+ */
+
+// Get colors for quiz form
+function getColors()
+{
+    return array('Blue', 'Black', 'White', 'Grey', 'Pine Green');
+}
